6bb52b456b18bdab2963743e7089e4c869df2bf9
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2022-03-09 14:48:57 +0100

    add cache

--- src/any-to-rdf-converter.ts
+++ src/any-to-rdf-converter.ts
@@ -5,11 +5,13 @@ import {
   NotFoundHttpError,
   readableToString,
   Representation,
-  RepresentationConverterArgs,
-  TypedRepresentationConverter,
+  RepresentationConverterArgs
 } from "@solid/community-server";
 import fs from "fs-extra";
-import { https } from "follow-redirects";
+import {https} from "follow-redirects";
+
+const crypto = require('crypto');
+const humanToMilliseconds = require('human-to-milliseconds')
 
 const RMLMapperWrapper = require("@rmlio/rmlmapper-java-wrapper");
 const outputType = "text/turtle";
@@ -25,61 +27,135 @@ const getSourceName = (contentType: string) =>
   `data.${contentType.split("/")[1]}`;
 
 export class AnyToRdfConverter extends BaseTypedRepresentationConverter {
-  private rmlRulesPath: string;
-  private rmlmapperPath: string;
+  private readonly rmlRulesPath: string;
+  private readonly rmlmapperPath: string;
+  private readonly cache: { [key: string]: any } | null;
+  private readonly cacheRetention: number | undefined;
+  private intervalID: NodeJS.Timer | undefined;
 
-  public constructor(rmlRulesPath: string, rmlmapperPath: string) {
+  public constructor(rmlRulesPath: string, rmlmapperPath: string, cache?: boolean, cacheRetention?: string, cacheCleanUp?: string) {
     super("application/json", outputType);
 
     this.rmlRulesPath = rmlRulesPath;
     this.rmlmapperPath = rmlmapperPath;
+    this.cache = cache ? {} : null;
+    cacheCleanUp = cacheCleanUp || '1m';
+
+    if (this.cache && cacheRetention !== 'infinity') {
+      this.cacheRetention = humanToMilliseconds(cacheRetention || '15m');
+      this.intervalID = setInterval(this._cleanUpCache.bind(this), humanToMilliseconds(cacheCleanUp));
+    }
   }
 
   public async handle({
-    identifier,
-    representation,
-  }: RepresentationConverterArgs): Promise<Representation> {
+                        identifier,
+                        representation,
+                      }: RepresentationConverterArgs): Promise<Representation> {
     if (representation.metadata.contentType === undefined)
       throw new InternalServerError("Content type can't be undefined");
 
     const data = await readableToString(representation.data);
+    let rdf = this._getRDFFromCache(data);
 
-    let rml;
-    try {
-      rml = await fs.readFile(this.rmlRulesPath, "utf-8");
-    } catch (error: any) {
-      if (error.code === "ENOENT")
-        throw new InternalServerError("RML file is not found");
-      else throw error;
-    }
+    if (!rdf) {
+      let rml;
+      try {
+        rml = await fs.readFile(this.rmlRulesPath, "utf-8");
+      } catch (error: any) {
+        if (error.code === "ENOENT")
+          throw new InternalServerError("RML file is not found");
+        else throw error;
+      }
 
-    const wrapper = new RMLMapperWrapper(this.rmlmapperPath, "./tmp", true);
+      const wrapper = new RMLMapperWrapper(this.rmlmapperPath, "./tmp", true);
 
-    let result;
-    try {
-      result = await wrapper.execute(rml, {
-        sources: { [getSourceName(representation.metadata.contentType)]: data },
-        generateMetadata: false,
-        serialization: "turtle",
-      });
-    } catch (error: any) {
-      if (error.toString() === FILE_NOT_FOUND) {
-        await this._download();
+      let result;
+      try {
         result = await wrapper.execute(rml, {
-          sources: {
-            [getSourceName(representation.metadata.contentType)]: data,
-          },
+          sources: {[getSourceName(representation.metadata.contentType)]: data},
           generateMetadata: false,
           serialization: "turtle",
         });
-      } else throw error;
+      } catch (error: any) {
+        if (error.toString() === FILE_NOT_FOUND) {
+          await this._download();
+          result = await wrapper.execute(rml, {
+            sources: {
+              [getSourceName(representation.metadata.contentType)]: data,
+            },
+            generateMetadata: false,
+            serialization: "turtle",
+          });
+        } else throw error;
+      }
+
+      rdf = result.output;
     }
 
+    if (!rdf) {
+      throw new Error(`AnyToRdfConverter: RDF is ${rdf}.`);
+    }
+
+    this._setRDFinCache(data, rdf);
+
     return new BasicRepresentation(
-      result.output,
+      rdf,
       representation.metadata,
       outputType
     );
+  }
+
+  _getRDFFromCache(data: string) {
+    if (this.cache) {
+      const hash: string = crypto.createHash('md5').update(data).digest("hex");
+      if (!this.cache[hash]) {
+        return null;
+      }
+
+      const {rdf} = this.cache[hash];
+
+      if (rdf) {
+        this.cache[hash].lastUsed = new Date();
+        console.log(`AnyToRdfConverter: Use RDF from cache for hash ${hash}`);
+      }
+
+      return rdf;
+    }
+
+    return null;
+  }
+
+  _setRDFinCache(data: string, rdf: string) {
+    if (this.cache) {
+      const hash: string = crypto.createHash('md5').update(data).digest("hex");
+      this.cache[hash] = {rdf, lastUsed: new Date()};
+    }
+  }
+
+  _cleanUpCache() {
+    const now = (new Date()).getTime();
+    // @ts-ignore
+    const hashes = Object.keys(this.cache);
+    // @ts-ignore
+    const usedAfter = now - this.cacheRetention;
+
+    hashes.forEach(hash => {
+      // @ts-ignore
+      const value = this.cache[hash];
+      const lastUsed = value.lastUsed.getTime();
+
+      if (lastUsed < usedAfter) {
+        console.log(`AnyToRdfConverter: Removing RDF for hash ${hash}`);
+        // @ts-ignore
+        delete this.cache[hash];
+      }
+    });
+  }
+
+  stopCacheCleanUps() {
+    if (this.intervalID) {
+      clearInterval(this.intervalID);
+    }
   }
 
   /**
@@ -93,7 +169,7 @@ export class AnyToRdfConverter extends BaseTypedRepresentationConverter {
           {
             host: RMLMAPPER_LATEST.host,
             path: RMLMAPPER_LATEST.path,
-            headers: { "User-Agent": "RMLMapper downloader" },
+            headers: {"User-Agent": "RMLMapper downloader"},
           },
           (res) => {
             let jsonString = "";
@@ -107,7 +183,7 @@ export class AnyToRdfConverter extends BaseTypedRepresentationConverter {
               while (
                 i < json.assets.length &&
                 json.assets[i].browser_download_url.indexOf(".jar") === -1
-              )
+                )
                 i++;
 
               if (i < json.assets.length) {
--- test/test.ts
+++ test/test.ts
@@ -1,29 +1,32 @@
 import { expect } from "chai";
-import { AnyToRdfConverter } from "../src/any-to-rdf-converter";
+import { AnyToRdfConverter } from "../src";
 
 import {
   guardedStreamFrom,
   RepresentationMetadata,
   readableToString,
-  BadRequestHttpError,
   InternalServerError,
 } from "@solid/community-server";
 import * as chai from "chai";
 import chaiAsPromised from "chai-as-promised";
 import fs from "fs-extra";
+import sinon from 'sinon';
 chai.use(chaiAsPromised);
 
 /**
- * Converts a JSON input to RDF and converts the stream to string
- * @param input - JSON to convert to RDF
- * @returns Converted input in string format
+ * Converts a JSON input to RDF and converts the stream to string.
+ * @param input - JSON to convert to RDF.
+ * @param converter - AnyToRdfConverter you want to reuse. If not provided a new one is created.
+ * @param cache - Use cache (optional, default is false).
+ * @returns Converted input in string format.
  */
-const convertJsonToRDF = async (input: any): Promise<any> => {
+const convertJsonToRDF = async (input: any, converter?: AnyToRdfConverter): Promise<any> => {
   const inputStream = guardedStreamFrom(JSON.stringify(input));
-  const outputStream = await new AnyToRdfConverter(
+  converter = converter || new AnyToRdfConverter(
     "./test/events/events.rml.ttl",
     "./rmlmapper.jar"
-  ).handle({
+  );
+  const outputStream = await converter.handle({
     identifier: { path: "json" },
     representation: {
       metadata: new RepresentationMetadata("application/json"),
@@ -36,22 +39,17 @@ const convertJsonToRDF = async (input: any): Promise<any> => {
   return await readableToString(outputStream.data);
 };
 
-describe("JsonToRdfConverter", function () {
-  this.timeout(10000);
-
-  describe("Verify convertor on correct input", () => {
-    it("#1 - json", async () => {
-      const input = {
-        name: "Test for Solid calendar",
-        events: [
-          {
-            title: "Correctly converted",
-            startDate: "2021-04-08T15:00:00.000Z",
-            endDate: "2021-04-08T17:00:00.000Z",
-          },
-        ],
-      };
-      const expectedResult = `
+const inputJson = {
+  name: "Test for Solid calendar",
+  events: [
+    {
+      title: "Correctly converted",
+      startDate: "2021-04-08T15:00:00.000Z",
+      endDate: "2021-04-08T17:00:00.000Z",
+    },
+  ],
+};
+const expectedResultFromJson = `
 <http://example.com/calendar/Test%20for%20Solid%20calendar> <http://schema.org/event>
     <http://example.com/event/Correctly%20converted>;
   <http://schema.org/name> "Test for Solid calendar" .
@@ -61,12 +59,19 @@ describe("JsonToRdfConverter", function () {
   <http://schema.org/name> "Correctly converted";
   <http://schema.org/startDate> "2021-04-08T15:00:00.000Z" .\n`;
 
-      const data = await convertJsonToRDF(input);
+describe("JsonToRdfConverter", function () {
+  this.timeout(20*1000);
 
-      expect(data).equal(expectedResult);
+  describe("Verify convertor on correct input", () => {
+    const consoleLogStub = sinon.stub(console, 'log');
+
+    it("JSON", async () => {
+      const data = await convertJsonToRDF(inputJson);
+
+      expect(data).equal(expectedResultFromJson);
     });
 
-    it("#2 - csv", async () => {
+    it("CSV", async () => {
       const expectedResult = `
 <http://example.com/movie/fotr> a <http://schema.org/Movie>;
   <http://example.com/year> "2001";
@@ -98,6 +103,36 @@ describe("JsonToRdfConverter", function () {
       const data = await readableToString(outputStream.data);
 
       expect(data).equal(expectedResult);
+    });
+
+    it("JSON with cache", async () => {
+      const converter = new AnyToRdfConverter(
+        "./test/events/events.rml.ttl",
+        "./rmlmapper.jar",
+        true,
+        'infinity'
+      );
+      const data = await convertJsonToRDF(inputJson, converter);
+      expect(data).equal(expectedResultFromJson);
+      const data2 = await convertJsonToRDF(inputJson, converter);
+      expect(data2).equal(expectedResultFromJson);
+      expect(consoleLogStub.withArgs(`AnyToRdfConverter: Use RDF from cache for hash 0dece66c971f9ad34333f6e97d5fd3ab`).callCount ).to.equal(1);
+      converter.stopCacheCleanUps();
+    });
+
+    it("Clean up cache", async () => {
+      const converter = new AnyToRdfConverter(
+        "./test/events/events.rml.ttl",
+        "./rmlmapper.jar",
+        true,
+        '6s',
+        '5s'
+      );
+      const data = await convertJsonToRDF(inputJson, converter);
+      expect(data).equal(expectedResultFromJson);
+      await sleep(13*1000)
+      expect(consoleLogStub.withArgs(`AnyToRdfConverter: Removing RDF for hash 0dece66c971f9ad34333f6e97d5fd3ab`).callCount ).to.equal(1);
+      converter.stopCacheCleanUps();
     });
   });
 
@@ -166,3 +201,7 @@ describe("JsonToRdfConverter", function () {
     });
   });
 });
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
